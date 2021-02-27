6157260cf790f854e08977fdea3f7a21792e71c1
leonardo silva <silvaleonardo.ti@gmail.com>
2021-02-27 08:43:25 -0300

    feat: Initial setup

---
+++ Models/BackendContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Models
+{
+    public class BackendContext : DbContext
+    {
+        public BackendContext(DbContextOptions<BackendContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
